34e5befd1eea043f05fcbdf736ea0e2d3a15dc84
Kennedy Njoroge <kennjoroges@gmail.com>
2021-04-26 11:36:31 +0300

    Heroku secret key config

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
